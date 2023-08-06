c7c966e7057c860e93ebb5ad35f9472b960e1d5e
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2023-08-06 15:09:11 +0530

    more api stuff

---
+++ base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# database stuff 
+# add whatever here , prefferably json based 
+
+
+
+
+# creating a model entry 
+# class Item(models.Model):
+#     # the parameters needed : 
+#     # add as needed 
+    
+#     name = models.CharField(max_length=200)
+#     created = models.DateTimeField(auto_created=True)
+    
